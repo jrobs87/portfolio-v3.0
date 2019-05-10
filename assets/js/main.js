6ed2f3d1ae54f7394d4f0999d12e539ee60c034d
John Robertson <tiktaalikdesign@gmail.com>
2019-05-10 13:03:22 -0400

    tons fo changes.  circle cursor, portflio modal, animations, colors, nav items change color on scroll, etc.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,106 @@
-$(document).ready(function () {
-  
+// ===== BEGIN SCROLLIFY-TRIGGERED CONTENT CHANGES ======================= //
+let sections = [
+  {
+    navColor: '#52489C'
+  },
+  {
+    navColor: '#339989'
+  },
+  {
+    navColor: '#171D1C'
+  },
+  {
+    navColor: '#3867B1'
+  },
+  {
+    navColor: '3867B1'
+  }
+];
 
-  // expand the nav
+let portfolio = [
+  {
+    name: 'Atlanta CFO Forum',
+    image: '',
+    description: 'Networking and job search platform for exectuive talent in Atlanta, GA.',
+    tech: ['HTML','JavaScript','CSS','PHP','Wordpress','Divi Builder'],
+    url: '',
+    github: '',
+  }
+]
+
+let sectionCurrent = 0;
+
+reveal = function () {
+  $('#about-splash').css('opacity', '1');
+  $('#about-splash').css('margin-top', '0');
+  console.log('heck');
+}
+
+// ===== END SCROLLIFY-TRIGGERED CONTENT CHANGES ========================= //
+
+$(document).ready(function () {
+
+  $('body').awesomeCursor('pencil');
+
+  // ===== BEGIN NAVIGATION MENU ========================================= //
   $('#nav-icon, .menu-row').on('click', function () {
+
+    // ----- Set navigation menu state thru the data-state attibute ------ //
     let state = $('#nav-icon').attr('data-state')
+
+    // ----- Nav state closed > expand nav ------------------------------- //
     switch (state) {
       case 'closed':
-        $('#nav-container').css('opacity', '1')
-        $('#nav-icon').attr('data-state', 'open');
+
+        // ----- Reveal nav with opacity and z-index incread ------------- // 
+        $('#nav-container').css('opacity', '1');
         $('#nav-main').css('z-index', '2');
         $('#nav-main').css('height', '100vh');
-        $('#nav-icon').css('color', '#F3F1F3')
+
+        // ----- Reset the menu button to alternate color for open state - //
+        $('#nav-icon').css('color', '#E1E4E9');
+
+        // ----- Set nav menu state to open ------------------------------ //
+        $('#nav-icon').attr('data-state', 'open');
+
+
         setTimeout(function () {
-          $('#nav-icon').css('dsiplay', '0.3s');
+          $('#nav-icon').css('display', '0.3s');
+          $('#nav-main').css('transition', '0.30s'); // TESTING - //
         }, 600);
         break;
+
+      // ----- Nav state open > nav state close - close nav -------------- //
       case 'open':
-      $('#nav-container').css('display', 'none');
+
+        // ----- Hide nav menu container first - before nav menu collapse  //
+        $('#nav-container').css('display', 'none');
+
+        // Reset Nav continer opacity (ready for expand nav animation ---- //
         $('#nav-container').css('opacity', '0')
+
+
+        // ----- Set menu nav state to closed ---------------------------- //
         $('#nav-icon').attr('data-state', 'closed');
-        $('#nav-icon').css('color', 'black')
+
+        $('#nav-icon').css('color', sections[sectionCurrent].navColor); // revert to current section nav-item color
+        $('#nav-main').css('height', '0');
+
         setTimeout(function () {
-          $('#nav-main').css('height', '0');
           $('#nav-icon').css('transition-delay', '0.1s');
+          // $('#nav-main').css('transition-delay', '0s');
           $('#nav-main').css('z-index', '-1');
           $('#nav-container').css('display', '');
-        }, 10);
+          $('#nav-main').css('transition', '0.20s'); // TESTING - //
+        }, 100);
         break;
-    }
+    };
   });
 
+  // ----- Menu row animations (numbers) ----------------------------------- //
   $('.menu-row').mouseover(function () {
     number = $(this).attr('data-row-number')
-    menuID = `#menu-${number}`;
+    menuID = `#menu-${number}`; // set here - no need to include in mouseout below
     $(menuID).css('opacity', '1');
   })
 
@@ -41,25 +109,96 @@ $(document).ready(function () {
     $('.menu-number').css('opacity', '0');
   });
 
+  // ----- Menu row click event ------------------------------------------- //
   $('.menu-row').on('click', function () {
     let move = $(this).attr('data-menu-target');
     $.scrollify.instantMove(`#${move}`);
-    console.log('Viewing ' + move + ' panel')
+    console.clear();
+
+    // set current section id to variable for css changes
+    current = $.scrollify.current();
+    console.log(current);
+    console.log(`#${current[0].id}`);
+    index = $.scrollify.currentIndex();
+    console.log(index);
+
+  });
+  // ===== END NAVIGATION MENU ============================================= //
+
+  // ===== BEGIN CONTENT SCRIPTS =========================================== //
+
+  // ----- Animate splash on load ------------------------------------------ //
+
+  // ----- Section objects ------------------------------------------------- //
+
+  $('#splash').css('opacity', '1');
+  $('#splash').css('color', 'black'); // SET TO THEME COLOR
+  $('#splash').css('top', '35%');
+
+  console.log("Good time of day new friend.")
+  console.log("My Name is John. Please enjoy my work.")
+
+  $('#scroll-down').on('click', function () {
+    $.scrollify.move("#about");
+  })
+
+  // $.scrollify.move(function () {
+  //   console.log('moved to ' + $.scrollify.currentIndex());
+  // });
+
+  
+
+  // ===== BEGIN MATERILIZE INIT ========================================== //
+  // modals (unused but initilialized)
+  // $('.modal').modal();
+
+  // collapse (unused but initilialized)
+  // $('.collapsible').collapsible();
+
+  // tabs (unused but initilialized)
+  // $('.tabs').tabs();
+
+  // scrollspy for portfolio section (unused but initilialized)
+  // $('.scrollspy').scrollSpy();
+
+  // carousel for portfolio
+  // $('.carousel').carousel();
+
+  // ===== END MATERILIZE INI ============================================ //
+
+  // ===== BEGIN EVENT LISTENERS ========================================= //
+
+  $('#click').on('click', function() {
+console.log('click');
+    $('#portfolio-overlay').css('display', 'block');
+    
+    setTimeout(function() {
+      $('#portfolio-overlay').css('opacity', 1);
+      $('#portfolio-overlay').css('bottom', 0);
+    },50);
   });
 
-  // animate splash on load
-  $('#splash').css('opacity', '1');
-  $('#splash').css('color', 'black');
-  $('#splash').css('top', '35%');
-console.log('Loaded JS...')
 
-// setTimeout(function() {
-//   $('body').removeClass('stop-scrolling');
-// },1000);
+    $('#close').on('click', function() {
+console.log('click');
+$('#portfolio-overlay').css('opacity', 0);
+// $('#portfolio-overlay').css('bottom', '-100vh');
+
+    
+    setTimeout(function() {
+      
+      $('#portfolio-overlay').css('display', 'none');
+    },250);
+
+    console.log('modal');
+    // // render the portfolio projects
+    // for (i = 0; i < portfolio.length; i++) {
+    //   let div = $('<div>');
+    //   $(div).text(`${portfolio[i].name} is a ${portfolio[i].description} `);
+    //   $('#portfolio-list').append(div);
+    // };
+
+    // $.scrollify.update();
+  });
 
 });
-
-
-
-
-
